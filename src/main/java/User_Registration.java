9d196b0b4593c119af0546b89de1fe7f1d463d18
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-04-01 22:56:04 +0530

    Added: Method to take first name as the input from the user

--- src/main/java/User_Registration.java
+++ src/main/java/User_Registration.java
@@ -1,8 +1,46 @@
-public class User_Registration {
-        public static void main(String [] args)
+import java.util.regex.*;
+import java.util.Scanner;
+public class User_Registration{
+    String firstName;
+    Scanner input=new Scanner(System.in);
+
+    //Method for taking first name as input
+    public String getFirstName()
+    {
+        System.out.println("Please Enter your first Name");
+        System.out.println("Rules: ");
+        System.out.println("First letter should be Capital letter and minimum 3 characters");
+        return input.next();
+    }
+
+    //Method for input checking
+    public void userValidator()
+    {
+        this.firstName=getFirstName();
+        boolean fName=Pattern.matches("^[A-Z][a-z]{2,}", firstName);
+        System.out.print("Your Entered name is ");
+        printingResult(fName);
+    }
+
+    public static void printingResult(boolean check)
+    {
+        if(Boolean.TRUE.equals(check))
         {
-            System.out.println("Welcome to the User Registration");
+            System.out.println("VALID");
+        }
+        else
+        {
+            System.out.println("INVALID");
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration");
+        User_Registration userinfo=new User_Registration();
+        userinfo.userValidator();
+    }
+}
+
+
 
 
