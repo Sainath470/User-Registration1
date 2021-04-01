bdf7a8542e71ab0efddfadf774b2f907796d9e8d
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-02 02:19:42 +0530

    Modified: method email to clear all email samples

--- src/main/java/User_Registration.java
+++ src/main/java/User_Registration.java
@@ -61,7 +61,7 @@ public class User_Registration{
         System.out.print("Your Entered last name is ");
         printingResult(lName);
         this.email = getUserEmail();
-        boolean emailId = Pattern.matches("^[a-zA-Z.]+([a-z]+)?[@][a-z]+\\.[a-z]{2,3}(\\.[A-Za-z]{2,6})?", email);
+        boolean emailId = Pattern.matches("^[\\D\\d.\\-\\+]+[@][a-z\\d]+\\.[a-z]{2,3}(\\.[a-z]{2,3})?", email);
         System.out.print("Your entered Email is ");
         printingResult(emailId);
         this.phoneNumber=getUserPhoneNumber();
