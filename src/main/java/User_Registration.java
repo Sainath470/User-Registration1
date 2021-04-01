0ac7e94406e3214460f4c5fd6e98b9d9ab184b83
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-04-01 22:41:32 +0530

    Added: Welcome to the User Registration program

---
+++ src/main/java/User_Registration.java
@@ -0,0 +1,8 @@
+public class User_Registration {
+        public static void main(String [] args)
+        {
+            System.out.println("Welcome to the User Registration");
+        }
+    }
+
+
